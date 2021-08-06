bc7b9b4df6a2c845d1a11c065b3c8d2d6437376f
Sam Scott <sam@osohq.com>
2021-08-05 22:42:38 -0400

    Got the basic extension running with the Rust server instead.

--- vscode/oso/client/src/extension.ts
+++ vscode/oso/client/src/extension.ts
@@ -2,32 +2,27 @@ import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
 
 import {
+	Executable,
 	LanguageClient,
 	LanguageClientOptions,
 	ServerOptions,
-	TransportKind
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
 	// The server is implemented in node
-	const serverModule = context.asAbsolutePath(
-		path.join('server', 'dist', 'server.js')
+	const serverBinary = context.asAbsolutePath(
+		path.join('..', '..', 'target', 'debug', 'polar-analyzer')
 	);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+	// // The debug options for the server
+	// // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+	// const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
+		command: serverBinary
 	};
 
 	// Options to control the language client
